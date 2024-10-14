79ac4105664517f77a6cdf5dcf7928b4473577be
VictorANT200 <20A3110452@untels.edu.pe>
2024-10-14 15:59:03 -0500

    PARTE 2

--- app/src/main/java/com/example/damep_rt/MainActivity.java
+++ app/src/main/java/com/example/damep_rt/MainActivity.java
@@ -8,17 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.Button;
+import android.widget.Toast;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button boton_enviar = findViewById(R.id.boton_enviar);
+        boton_enviar.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
+
     }
 }
